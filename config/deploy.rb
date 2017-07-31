193083640a181872f052e3103b5867dd33e1e85d
centrevillage <centrevillage@gmail.com>
2017-07-31 22:40:57 +0900

    fix rbenv type

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,7 +11,7 @@ set :user, "ec2-user"
 set :group, "ec2-user"
 
 set :rbenv_custom_path, '/usr/local/rbenv'
-set :rbenv_type, :system
+set :rbenv_type, :user
 set :rbenv_ruby, '2.3.4'
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_map_bins, %w{rake gem bundle ruby rails}
