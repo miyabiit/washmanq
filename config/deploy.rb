70b902a05bf7cf91b8af29c71deb2e56e4d121b1
centrevillage <centrevillage@gmail.com>
2017-07-31 22:46:00 +0900

    fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,7 +14,7 @@ set :rbenv_custom_path, '/usr/local/rbenv'
 set :rbenv_type, :user
 set :rbenv_ruby, '2.3.4'
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
-set :rbenv_map_bins, %w{rake gem bundle ruby rails}
+set :rbenv_map_bins, %w{rake gem bundle ruby rails puma}
 set :rbenv_roles, :all # default value
 
 set :nvm_type, :user # or :system, depends on your nvm setup
