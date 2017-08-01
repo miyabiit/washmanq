91371eabbb34e8ca22f6e86a12f67bcf1c5d5cc5
centrevillage <centrevillage@gmail.com>
2017-08-01 14:44:27 +0900

    add basic auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  unless Rails.env.development? || Rails.env.test?
+    http_basic_authenticate_with name: ENV['BASIC_AUTH_USER'], password: ENV['BASIC_AUTH_PASSWORD']
+  end
 end
