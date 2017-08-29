68f44e9ad101fce869089811417cba7029729b81
centrevillage <centrevillage@gmail.com>
2017-08-29 10:41:07 +0900

    issue #8 統計情報の過去一年推移に合計タブを追加

--- app/models/sales_summary.rb
+++ app/models/sales_summary.rb
@@ -1,4 +1,7 @@
 class SalesSummary < ApplicationRecord
   belongs_to :place
 
+  scope :sum_amount_and_count_by_target_month, -> {
+    group(:target_month).select("target_month, SUM(sales_count) AS sales_count_sum, SUM(sales_amount) AS sales_amount_sum")
+  }
 end
--- spec/logics/sales_calculator_spec.rb
+++ spec/logics/sales_calculator_spec.rb
@@ -121,4 +121,42 @@ describe SalesCalculator do
       expect(spray_sales_summaries.last.prepaid_sales_amount).to eq(201)
     end
   end
+
+  describe '#all_summary_with_rate_for_transition' do
+    before do
+      @places = (1..2).map{|i| Place.create!(name: "テスト#{i}") }
+      other_place_sumamry = SalesSummary.create!(place: Place.create!(name: '対象'), 
+                                                 target_month: '201708', sales_count: 11, sales_amount: 111)
+      start_date = Date.new(2016, 7, 1)
+      @summaries = (0..13).map {|i|
+        SalesSummary.create!(place: @places[rand(2)], target_month: start_date.since(i.month).strftime('%Y%m'),
+                             sales_count: 1*(i+1), sales_amount: 100*(i+1))
+      }
+      start_date = start_date.prev_year
+      @prev_year_summaries = @summaries.take(12).map.with_index {|s, i|
+        SalesSummary.create!(place: s.place, target_month: start_date.since(i.month).strftime('%Y%m'),
+                            sales_count: 10*(i+1), sales_amount: 200*(i+1))
+      }
+    end
+
+    it do
+      summaries = SalesCalculator.all_summary_with_rate_for_transition(Date.new(2017, 8, 1)).to_a
+      expect(summaries.count).to eq(14)
+      expect(summaries.last.target_month).to eq('201708')
+      expect(summaries.last.sales_count).to eq(1*14 + 11)
+      expect(summaries.last.sales_count_prev_year_rate).to eq(25.0 / 2.0)
+      expect(summaries.last.sales_count_prev_month_rate).to eq(25.0 / 13.0)
+      expect(summaries.last.sales_amount).to eq(100*14 + 111)
+      expect(summaries.last.sales_amount_prev_year_rate).to eq(1511.0 / 200.0)
+      expect(summaries.last.sales_amount_prev_month_rate).to eq(1511.0 / 1300.0)
+
+      expect(summaries.first.target_month).to eq('201607')
+      expect(summaries.first.sales_count).to eq(1)
+      expect(summaries.first.sales_count_prev_year_rate).to eq(1.0 / 10.0)
+      expect(summaries.first.sales_count_prev_month_rate).to eq(1.0 / 120.0)
+      expect(summaries.first.sales_amount).to eq(100)
+      expect(summaries.first.sales_amount_prev_year_rate).to eq(0.5)
+      expect(summaries.first.sales_amount_prev_month_rate).to eq(100.0 / (200.0*12))
+    end
+  end
 end
