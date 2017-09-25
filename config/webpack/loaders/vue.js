6256956dba135c9ebf2ab66c29b0ae970c32992d
centrevillage <centrevillage@gmail.com>
2017-09-25 16:24:38 +0900

    fix vue loader

--- config/webpack/loaders/vue.js
+++ config/webpack/loaders/vue.js
@@ -1,12 +1,14 @@
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const path = require('path')
 const { env } = require('../configuration.js')
+const postcssConfig = path.resolve(process.cwd(), '.postcssrc.yml')
 
 // Change it to false if you prefer Vue styles to be inlined by javascript in runtime
 const extractStyles = false
 
 const cssLoader = [
   { loader: 'css-loader', options: { minimize: env.NODE_ENV === 'production' } },
-  { loader: 'postcss-loader', options: { sourceMap: true } },
+  { loader: 'postcss-loader', options: { sourceMap: true, config: { path: postcssConfig } } },
   'resolve-url-loader'
 ]
 const sassLoader = cssLoader.concat([
