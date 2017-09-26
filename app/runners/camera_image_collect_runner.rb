0187ab8f4f7f016e25c31ebf96f2f29389a195e8
centrevillage <centrevillage@gmail.com>
2017-09-26 16:18:30 +0900

    fix time parsing

--- app/runners/camera_image_collect_runner.rb
+++ app/runners/camera_image_collect_runner.rb
@@ -4,7 +4,7 @@ class CameraImageCollectRunner
     
     Camera.all.each do |camera|
       Dir[File.join(base_path, camera.name, '*')].each do |file_name|
-        shooted_at = Time.strptime(File.basename(file_name).scan(/\d{17,17}/).first, '%Y%m%d%H%M%S%L')
+        shooted_at = Time.zone.strptime(File.basename(file_name).scan(/\d{17,17}/).first, '%Y%m%d%H%M%S%L')
         unless camera.camera_images.where(shooted_at: shooted_at).exists?
           File.open(file_name) do |file|
             camera_image = CameraImage.create!(
