0e4a02ea9da90279287ed688e5d37556273e635c
centrevillage <centrevillage@gmail.com>
2019-08-28 11:24:22 +0900

    issue #36 画像取り込みバッチ中で対象ファイルが壊れていたらスキップするように

--- app/runners/camera_image_collect_runner.rb
+++ app/runners/camera_image_collect_runner.rb
@@ -6,13 +6,18 @@ class CameraImageCollectRunner
       Dir[File.join(base_path, camera.name, '*')].each do |file_name|
         shooted_at = Time.zone.strptime(File.basename(file_name).scan(/\d{17,17}/).first, '%Y%m%d%H%M%S%L')
         unless camera.camera_images.where(shooted_at: shooted_at).exists?
-          File.open(file_name) do |file|
-            camera_image = CameraImage.create!(
-              camera: camera,
-              shooted_at: shooted_at,
-              image: file
-            )
-            CameraChannel.broadcast_to_camera camera
+          begin
+            File.open(file_name) do |file|
+              camera_image = CameraImage.create!(
+                camera: camera,
+                shooted_at: shooted_at,
+                image: file
+              )
+              CameraChannel.broadcast_to_camera camera
+            end
+          rescue ActiveRecord::RecordInvalid => ex
+            puts "[ERROR] processing file = ${file_name}"
+            puts ex.message
           end
         end
         File.delete(file_name)
